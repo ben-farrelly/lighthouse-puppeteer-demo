9e722700aca121dc512876f2251836b7b113613a
ben-farrelly <benjamin.farrelly@serato.com>
2020-08-05 14:14:49 +1200

    Playig around with possible ways for perf testing with puppeteer

--- lighthouse-puppeteer/lighthouse-tests.js
+++ lighthouse-puppeteer/lighthouse-tests.js
@@ -9,11 +9,24 @@ const fs = require('fs');
 
 (async() => {
 
-    const loginURL = 'https://idp.nature.com/login/natureuser';
+    // const loginURL = 'https://serato.com/'; //https://idp.nature.com/login/natureuser';
     const logoutURL = 'https://idp.nature.com/logout/natureuser?redirect_uri=https%3A%2F%2Fwww.nature.com';
 
+    const testingUrls = [
+        'https://serato.com/',
+        'https://serato.com/dj/pro',
+        'https://serato.com/dj/pricing',
+        'https://serato.com/studio',
+        'https://serato.com/sounds',
+        'https://serato.com/studio/downloads',
+        'https://serato.com/beatfighter',
+        'https://serato.com/campaign/play-for-pro',
+        'https://serato.com/checkout/cart/item/add/58',
+        'https://serato.com/login'
+    ];
+    // todo - add scripts for authenticated views. My Account, Payment page?
     const opts = {
-        //chromeFlags: ['--headless'],
+        chromeFlags: ['--headless'],
         logLevel: 'info',
         output: 'json',
         disableDeviceEmulation: true,
@@ -21,7 +34,6 @@ const fs = require('fs');
             width: 1200,
             height: 900
         },
-        chromeFlags: ['--disable-mobile-emulation']
     };
 
 // Launch chrome using chrome-launcher
@@ -34,46 +46,60 @@ const fs = require('fs');
     const browser = await puppeteer.connect({browserWSEndpoint: webSocketDebuggerUrl});
 
 
-//Puppeteer
     page = (await browser.pages())[0];
-    await page.setViewport({ width: 1200, height: 900});
-    await page.goto(loginURL, {waitUntil: 'networkidle2'});
-    await page.type('[id="login-username"]', 'usernature@yopmail.com');
-    await page.type('[id="login-password"]', 'password1234');
-    await page.evaluate(() => {
-        document.querySelector('[id="login-submit"]').click();
-    });
+    await page.setViewport({ width: 1200, height: 900 });
 
-    await page.waitForNavigation();
+    for (let index = 0; index < testingUrls.length; index++) {
+        let url = testingUrls[index];
+        
+        
+    //Puppeteer
+        
+        await page.goto(url, {waitUntil: 'networkidle2'});
+        // await page.type('[id="email"]', 'benjamin.farrelly@serato.com');
+        // await page.type('[id="password"]', 'Quilombo19');
+        // await page.evaluate(() => {
+        //     document.querySelector('[type="submit"]').click();
+        // });
 
-    console.log(page.url());
+        // await page.waitForNavigation();
 
-// Run Lighthouse.
-    const report = await lighthouse(page.url(), opts, config).then(results => {
-        return results;
-    });
-    const html = reportGenerator.generateReport(report.lhr, 'html');
-    const json = reportGenerator.generateReport(report.lhr, 'json');
+        console.log(page.url());
 
-    // console.log(`Lighthouse score: ${report.lhr.score}`);
-    await page.goto(logoutURL, {waitUntil: 'networkidle2'});
+    // Run Lighthouse.
+        try {
+            const report = await lighthouse(page.url(), opts, config).then(results => {
+                return results;
+            });
+            const html = reportGenerator.generateReport(report.lhr, 'html');
+            const json = reportGenerator.generateReport(report.lhr, 'json');
+
+            // console.log(`Lighthouse score: ${report.lhr.score}`);
+            // await page.goto(logoutURL, {waitUntil: 'networkidle2'});
+            //Write report html to the file
+
+            // Todo push these reports into a manipulation queue
+            fs.writeFile('report-' + index + '.html', html, (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+
+            //Write report json to the file
+            fs.writeFile('report-' + index + '.json', json, (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+        } catch (err) {
+            console.error(err)
+        }
+    }
 
     await browser.disconnect();
     await chrome.kill();
 
 
-    //Write report html to the file
-    fs.writeFile('report.html', html, (err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
-
-    //Write report json to the file
-    fs.writeFile('report.json', json, (err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
+   
 
 })();
